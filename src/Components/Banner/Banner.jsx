23b462e88f093364c34a8cce645f9b9659876bb8
Luzmila Quezada <radiocat.fanzine@gmail.com>
2025-10-30 01:49:41 -0500

    primer commit

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner-container">
+                <h1>Simaru</h1>
+                <p>Sustainable fashion since 2017</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
