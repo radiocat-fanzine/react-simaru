abc9ed580462755709a5bf85f8cff384e97b0b6f
Luzmila Quezada <radiocat.fanzine@gmail.com>
2025-11-02 13:39:36 -0500

    avance 3

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Banner from "../Banner/Banner"
+import NavBar from "../Navbar/Navbar"
 
 const Home = () => {
     return(
+        <>
+        <NavBar/>
         <Banner/>
+        </>
     )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+        <nav className="navbar">
+            <h1 className="navbar-logo">Simaru.de</h1>
+            <h2 className="navbar-cart">🛒</h2>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
