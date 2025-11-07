1d4ed1771f0d2bdd787342d7cc52a347cfbad246
Luzmila Quezada <radiocat.fanzine@gmail.com>
2025-11-07 18:46:43 -0500

    actualizacion navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,32 +9,46 @@ function NavBar() {
 
   return (
     <nav className="nav-menu">
-      <Link to="/" className="nav-logo">
+      <div className="navbar-top-row">
+        <Link to="/" className="nav-logo">
         <img className="logo-image" src="./imgs/logo.png" alt="Simaru Logo" />
-      </Link>
+        </Link>
 
-      <div className="navbar-search">
-        <i className="fa fa-search"></i>
-        <input
-          type="text"
-          placeholder="Search eco-friendly products..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
+        <div className="nav-right-group">
+
+          <div className="navbar-search-wrapper">
+            <div className="navbar-search">
+              <input
+                type="text"
+                placeholder="Find your next piece here…"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <button className="search-icon-button" aria-label="Search">
+                <i className="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
+        
+          <Link to="/cart" className="nav-cart">
+            <CartWidget />
+          </Link>
+          
+        </div>
+
       </div>
-
-      <Link to="/cart" className="nav-cart">
-        <CartWidget />
-      </Link>
 
       <div className="nav-divider"></div>
 
-      <ul className="nav-links">
-        <li><Link to="/category/wallets">Wallets</Link></li>
-        <li><Link to="/category/bracelets">Bracelets</Link></li>
-        <li><Link to="/category/bags-pouches">Bags & Pouches</Link></li>
-        <li><Link to="/category/belts">Belts</Link></li>
-      </ul>
+      <div className="navbar-bottom-row">
+        <ul className="nav-links">
+          <li><Link to="/category/wallets">Wallets</Link></li>
+          <li><Link to="/category/bracelets">Bracelets</Link></li>
+          <li><Link to="/category/bags-pouches">Bags & Pouches</Link></li>
+          <li><Link to="/category/belts">Belts</Link></li>
+        </ul>
+      </div>
+
     </nav>
   );
 }
